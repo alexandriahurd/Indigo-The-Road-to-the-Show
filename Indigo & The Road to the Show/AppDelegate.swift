40fd35e38c9879992adaddc229655f65311db103
Game Design Shared <GameDesignShared@mac-lab-d25tq02lgg7d.local>
2019-03-28 14:42:10 -0500

    Initial Commit

---
+++ Indigo & The Road to the Show/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Indigo & The Road to the Show
+//
+//  Created by Game Design Shared on 3/28/19.
+//  Copyright © 2019 Alexandria Hurd. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
